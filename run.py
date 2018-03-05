2e09957a0aea7bc50a263490c077813f5f9e8805
Collins njau <collinsnjau39@gmail.com>
2018-03-05 21:49:15 +0300

    Create the Business class

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 
 from app import create_app
 
-config_name=os.getenv('APP_SETTINGS')#currently using "development"
+config_name=os.getenv('APP_SETTINGS')#currently using development settings
 
 app=create_app(config_name)
 
