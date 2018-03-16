3f9bc8ae28b914605169d2e7db85e9184c32f207
Collins njau <collinsnjau39@gmail.com>
2018-03-16 13:49:53 +0300

    Test if a user can add a review

--- test_business_reviews.py
+++ test_business_reviews.py
@@ -7,6 +7,9 @@ from app.models import Review, Business
 
 class ReviewTestCase(unittest.TestCase):
     """the class to test the reviews"""
+    """first create business"""
+    """get the business id"""
+    """use it to create the review"""
 
     def setUp(self):
         """this test will run before every test"""
@@ -26,3 +29,39 @@ class ReviewTestCase(unittest.TestCase):
         #test if the api can create a business 
         res=self.client().post('/api/v1/businesses', data=self.business)
         self.assertEqual(res.status_code,201)
+
+
+    def test_api_create_review(self):
+        #first add the business
+        res=self.client().post('/api/v1/businesses', data=self.business)
+        self.assertEqual(res.status_code,201)
+        #get the id of the created business
+        res_in_json=json.loads(res.data.decode('utf-8').replace("'", "\""))
+        #make post request to add review
+        result=self.client().post('api/v1/businesses/{}/reviews'.format(res_in_json['id']), data=self.review)
+        self.assertEqual(result.status_code,201)
+        
+
+
+
+
+
+
+    def tearDown(self):
+        #runs after every test
+        #makes the business_list  and reviews_list empty
+        Business.business_list=[]
+        Review.review_list=[]     
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()         
